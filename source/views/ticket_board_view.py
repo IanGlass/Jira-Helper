cbfb1f420d0c2d4a1fa8a300326183295c22175c
IanGlass <37228869+IanGlass@users.noreply.github.com>
2018-12-06 12:50:46 +1300

    Add reporter to ticket board display, closes #29

--- source/views/ticket_board_view.py
+++ source/views/ticket_board_view.py
@@ -48,31 +48,37 @@ class TicketBoardView(QWidget):
         key_header.setText("Ticket Number")
         ticket_board_layout.addWidget(key_header, 1, 0)
 
+        reporter_heading = QLabel()
+        reporter_heading.setFont(header_font)
+        reporter_heading.setText("Reporter")
+        ticket_board_layout.addWidget(reporter_heading, 1, 1)
+
         summary_header = QLabel()
         summary_header.setFont(header_font)
         summary_header.setText("Summary")
-        ticket_board_layout.addWidget(summary_header, 1, 1)
+        ticket_board_layout.addWidget(summary_header, 1, 2)
 
         assigned_header = QLabel()
         assigned_header.setFont(header_font)
         assigned_header.setText("Assignee")
-        ticket_board_layout.addWidget(assigned_header, 1, 2)
+        ticket_board_layout.addWidget(assigned_header, 1, 3)
 
         last_updated_header = QLabel()
         last_updated_header.setFont(header_font)
         last_updated_header.setText("Last Updated")
-        ticket_board_layout.addWidget(last_updated_header, 1, 3)
+        ticket_board_layout.addWidget(last_updated_header, 1, 4)
 
         sla_header = QLabel()
         sla_header.setFont(header_font)
         sla_header.setText("Open for")
-        ticket_board_layout.addWidget(sla_header, 1, 4)
+        ticket_board_layout.addWidget(sla_header, 1, 5)
 
         # Build the ticket board
         self.col_key = list()
-        self.col_assigned = list()
+        self.col_reporter = list()
         self.col_summary = list()
         self.col_last_updated = list()
+        self.col_assigned = list()
         self.col_sla = list()
         text_font = QFont("Times", 12)
         for i in range(0, BOARD_SIZE):
@@ -80,27 +86,32 @@ class TicketBoardView(QWidget):
             self.col_key[i].setFont(text_font)
             ticket_board_layout.addWidget(self.col_key[i], i + 2, 0)
 
+            self.col_reporter.append(QLabel())
+            self.col_reporter[i].setFont(text_font)
+            ticket_board_layout.addWidget(self.col_reporter[i], i + 2, 1)
+
             self.col_summary.append(QLabel())
             self.col_summary[i].setFont(text_font)
-            ticket_board_layout.addWidget(self.col_summary[i], i + 2, 1)
+            ticket_board_layout.addWidget(self.col_summary[i], i + 2, 2)
 
             self.col_assigned.append(QLabel())
             self.col_assigned[i].setFont(text_font)
-            ticket_board_layout.addWidget(self.col_assigned[i], i + 2, 2)
+            ticket_board_layout.addWidget(self.col_assigned[i], i + 2, 3)
 
             self.col_last_updated.append(QLabel())
             self.col_last_updated[i].setFont(text_font)
-            ticket_board_layout.addWidget(self.col_last_updated[i], i + 2, 3)
+            ticket_board_layout.addWidget(self.col_last_updated[i], i + 2, 4)
 
             self.col_sla.append(QLabel())
             self.col_sla[i].setFont(text_font)
-            ticket_board_layout.addWidget(self.col_sla[i], i + 2, 4)
+            ticket_board_layout.addWidget(self.col_sla[i], i + 2, 5)
 
         self.red_phase = False  # Used to flash rows if red alert
 
     def clear_widgets(self):  # Ensures table is cleared if less than BOARD_SIZE issues are overdue
         for i in range(0, BOARD_SIZE):
             self.col_key[i].clear()
+            self.col_reporter[i].clear()
             self.col_summary[i].clear()
             self.col_assigned[i].clear()
             self.col_last_updated[i].clear()
@@ -138,6 +149,7 @@ class TicketBoardView(QWidget):
                 if (last_updated > settings.black_alert and count <= BOARD_SIZE):  # Only display if board is not full
                     if (last_updated > settings.melt_down):  # Things are serious!
                         self.col_key[count].setStyleSheet('color: red')
+                        self.col_reporter[count].setStyleSheet('color: red')
                         self.col_summary[count].setStyleSheet('color: red')
                         self.col_assigned[count].setStyleSheet('color: red')
                         self.col_last_updated[count].setStyleSheet('color: red')
@@ -145,23 +157,27 @@ class TicketBoardView(QWidget):
                     elif (last_updated > settings.red_alert):  # Things are not so Ok
                         if (self.red_phase):
                             self.col_key[count].setStyleSheet('color: red')
+                            self.col_reporter[count].setStyleSheet('color: red')
                             self.col_summary[count].setStyleSheet('color: red')
                             self.col_assigned[count].setStyleSheet('color: red')
                             self.col_last_updated[count].setStyleSheet('color: red')
                             self.col_sla[count].setStyleSheet('color: red')
                         else:
                             self.col_key[count].setStyleSheet('color: black')
+                            self.col_reporter[count].setStyleSheet('color: black')
                             self.col_summary[count].setStyleSheet('color: black')
                             self.col_assigned[count].setStyleSheet('color: black')
                             self.col_last_updated[count].setStyleSheet('color: black')
                             self.col_sla[count].setStyleSheet('color: black')
                     else:  # Things are still Okish
                         self.col_key[count].setStyleSheet('color: black')
+                        self.col_reporter[count].setStyleSheet('color: black')
                         self.col_summary[count].setStyleSheet('color: black')
                         self.col_assigned[count].setStyleSheet('color: black')
                         self.col_last_updated[count].setStyleSheet('color: black')
                         self.col_sla[count].setStyleSheet('color: black')
                     self.col_key[count].setText(str(support_ticket.key))
+                    self.col_key[count].setText(str(support_ticket.fields.reporter))
                     self.col_summary[count].setText(str(support_ticket.fields.summary))
                     self.col_assigned[count].setText(str(support_ticket.fields.assignee))
                     self.col_last_updated[count].setText(str(support_ticket.fields.updated))
